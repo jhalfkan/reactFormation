b91cc252c57e2223080c34d32f6c52d91180815a
stagiaire <stagiaire@cpg-453079.cepegra2008.lan>
2018-10-23 10:23:35 +0200

    Prepare onchange event + fix warning on link

--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NewsArticle = (props) => {
     return ( 
         <article style={props.style}>
-            <a href={props.content.url} target="_blank">
+            <a href={props.content.url} target="_blank" rel="noopener noreferrer">
                 <img src={props.content.urlToImage} alt={props.content.title} />
                 <h1>{props.content.title}</h1>
             </a>
--- src/components/NewsSources.js
+++ src/components/NewsSources.js
@@ -4,11 +4,15 @@ class NewsSources extends Component {
     state = {  }
     render() { 
         return ( 
-            <select>
+            <select onChange={this.handleChange}>
                 <option value='hacker-news'>Hacker News</option>
                 <option value='le-monde'>Le monde</option>
             </select>
          );
+    }
+
+    handleChange = (ev) => {
+        console.log('Changement de source');
     }
 }
  
