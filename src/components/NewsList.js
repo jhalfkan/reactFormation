268259d943d71cbd0e8ae6516935d62348a731d3
stagiaire <stagiaire@cpg-453079.cepegra2008.lan>
2018-10-23 10:56:08 +0200

    fetch data from api

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NewsArticle from './NewsArticle';
 import uuidv1 from 'uuid';
+import axios from 'axios';
 
 class NewsList extends Component {
     constructor(){
@@ -21,6 +22,24 @@ class NewsList extends Component {
                 )}
             </div>
          );
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    loadData = async () => {
+        try {
+            const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9668f23847a440f4b4d4fcf773547862');
+            console.log(response);
+            if(response.status === 200){
+                this.setState({
+                    data: response.data.articles
+                })
+            }
+        } catch (error) {
+            console.log('Something wrong with fetching: ' + error);
+        }
     }
 
     /* STYLE SHEET */
